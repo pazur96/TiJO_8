cf8b33c9becf2eccab1fd9d5ea2d1e62b21a6d18
”Jan <szpala27189@student.pwsztar.edu.pl>
2017-06-06 20:41:21 +0200

    exercise 2

--- test/app.spec.js
+++ test/app.spec.js
@@ -14,10 +14,25 @@ describe('reverseNumber', function ()
     {
         expect(app.reverseNumber(76)).to.eql(67);
     });
+
+    it('if is not a number', function()
+           {
+                expect(app.reverseNumber('yyyy4')).to.eql(false);
+        });
 });
 
 describe('returnOnlyLetter', function ()
 {
+    it('should return only letters', function()
+    {
+        expect(app.returnOnlyLetter(09775)).to.eql(false);
+    });
+
+    it('should return only letters', function()
+    {
+        expect(app.returnOnlyLetter('eer')).to.eql('eer');
+    });
+
     it('should return only letters', function()
     {
         expect(app.returnOnlyLetter('g3rsw1.')).to.eql('grsw');
@@ -27,6 +42,10 @@ describe('returnOnlyLetter', function ()
 
 describe('isEmail', function ()
 {
+    it('should return false when it is not a email', function()
+    {
+        expect(app.isEmail('qq23earfc.pl')).to.eql(false);
+    });
     it('should return true when is email', function()
     {
         expect(app.isEmail('qq23ea@rfc.pl')).to.eql(true);
