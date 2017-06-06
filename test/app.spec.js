06329918397863f754b54ccb63fa8b05feb81aa0
”Jan <szpala27189@student.pwsztar.edu.pl>
2017-06-06 20:35:36 +0200

    first report

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,17 +3,32 @@
 var expect = require('chai').expect;
 var app = require('../app/app');
 
+'use strict';
+
+var expect = require('chai').expect;
+var app = require('../app/app');
+
 describe('reverseNumber', function ()
 {
-
+    it('should revert 76 to 67', function()
+    {
+        expect(app.reverseNumber(76)).to.eql(67);
+    });
 });
 
 describe('returnOnlyLetter', function ()
 {
+    it('should return only letters', function()
+    {
+        expect(app.returnOnlyLetter('g3rsw1.')).to.eql('grsw');
+    });
 
 });
 
 describe('isEmail', function ()
 {
-
+    it('should return true when is email', function()
+    {
+        expect(app.isEmail('qq23ea@rfc.pl')).to.eql(true);
+    });
 });
